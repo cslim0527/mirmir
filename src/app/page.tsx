6166754bb6c55a21d1dbe3480a89dd5f4b37ae8d
임찬수 <cs.lim@hanssem.com>
2025-01-03 13:29:31 +0900

    Chore: Next, Radix UI 설치

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Container, Flex, Text, Button } from "@radix-ui/themes";
+
+export default function Home() {
+  return (
+    <Container size={{ sm: "1", lg: "4" }} px="16px">
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix</Text>
+        <Button>Lets go</Button>
+      </Flex>
+    </Container>
+  );
+}
